41d5ca3b2f9df0c2c534e6a6fb1fd8e69308bd21
Shadi Rada <shakkyr@gmail.com>
2021-12-31 00:33:34 +0200

    database create

---
+++ database/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://shakkyr:test123@reaturant-mern.g6per.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology : true
+    })
+
+        console.log('Database connection success');
+    }catch(err) {
+        console.log(err);
+    }
+}
+
+module.exports = connectDB
